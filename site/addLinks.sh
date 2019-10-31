878a107f8ec695c7f662adabeb15de0a700e46c5
Marc DiPasquale <Marc.DiPasquale@solace.com>
2019-10-31 10:26:37 -0400

    Change to relative paths

---
+++ site/addLinks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ln -s ../elements dist/workshops/elements
+ln -s ../workshops dist/workshops/workshops
+ln -s ../images dist/workshops/images
+ln -s ../site.webmanifest dist/workshops/site.webmanifest
+ln -s ../scripts dist/workshops/scripts
+ln -s ../styles/ dist/workshops/styles
+
+ln -s ../elements dist/iguides/elements
+ln -s ../iguides dist/iguides/iguides
+ln -s ../images dist/iguides/images
+ln -s ../site.webmanifest dist/iguides/site.webmanifest
+ln -s ../scripts dist/iguides/scripts
+ln -s ../styles/ dist/iguides/styles
